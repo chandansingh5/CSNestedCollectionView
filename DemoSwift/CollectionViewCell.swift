09c54a7cd046d79d59d5cbf4e3d1d8566ad627df
chandan <chandansingh.it13@gmail.com>
2017-03-01 02:45:21 +0530

    first commit

---
+++ DemoSwift/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  DemoSwift
+//
+//  Created by Chandan Singh on 28/02/17.
+//  Copyright © 2017 Chandan Singh. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  DemoSwift
+//
+//  Created by Chandan Singh on 28/02/17.
+//  Copyright © 2017 Chandan Singh. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
